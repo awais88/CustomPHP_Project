507ea8b7fa1bbbefec29a1d19f3e8cfe85e07847
awais88 <m.awaisaltaf@gmail.com>
2015-11-28 18:20:33 +0500

    Create side_bar.php

---
+++ resources/templates/side_bar.php
@@ -0,0 +1,7 @@
+<div class="side_bar">
+	<ul id="categories">
+		<li><a href="#">Category One</a></li>
+		<li><a href="#">Category Two</a></li>
+		<li><a href="#">Category Three</a></li>
+	</ul>
+</div><!-- side_bar -->
